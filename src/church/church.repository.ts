6432d47f161cb9b27258d418381d3a72f99c36b0
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-04-20 16:30:44 -0300

    feat: add churchProfile type in return of repository function

--- src/church/church.repository.ts
+++ src/church/church.repository.ts
@@ -2,13 +2,14 @@ import { Injectable } from "@nestjs/common";
 import { CreateChurchDto } from "./dto/create-church.dto";
 import { ChurchRepositoryInterface } from "./interface/church.repository.interface";
 import { PrismaService } from "src/database/prisma.service";
+import { ChurchProfile } from "@prisma/client";
 
 @Injectable()
 export class ChurchRepository implements ChurchRepositoryInterface {
     
     constructor(private prisma: PrismaService) {}
 
-    async create(data: CreateChurchDto): Promise<any> {    
+    async create(data: CreateChurchDto): Promise<ChurchProfile> {    
         return await this.prisma.churchProfile.create({ data });
     }
 
--- src/church/interface/church.repository.interface.ts
+++ src/church/interface/church.repository.interface.ts
@@ -1,5 +1,6 @@
+import { ChurchProfile } from "@prisma/client";
 import { CreateChurchDto } from "../dto/create-church.dto";
 
 export interface ChurchRepositoryInterface {
-    create(churchData: CreateChurchDto): Promise<any>;
+    create(churchData: CreateChurchDto): Promise<ChurchProfile>;
 }
